ced4006a002b2432af3795a6ae8634cfd0ef2b65
kambleadarsha98@gmail.com <kambleadarsha98@gmail.com>
2025-11-29 23:54:02 +0530

    new -patch

---
+++ script.js
@@ -0,0 +1,9 @@
+const  search =  document.getElementById("search"); 
+
+search.onclick =   function(){
+    let input =   document.getElementById("input"); 
+
+    input.style.visibility =   "visible";
+}
+
+
